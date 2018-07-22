00dfec5dd453412a6c3ecc43ab5b05e88de0739c
samuraikun <tazitaziawawa@gmail.com>
2018-07-22 17:53:56 +0900

    simple blog app

---
+++ src/components/post_show.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchPost, deletePost } from '../actions';
+
+class PostShow extends Component {
+  conmponentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
+  }
+  
+  render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>
+    }
+
+    return (
+      <div>
+        <Link to="/">Back To Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick}
+        >
+          Delete
+        </button>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ posts }, ownProps) {
+  return { posts: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostShow);
